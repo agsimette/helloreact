6971691825bce54eba3da7919315aadeb18356fc
agsimette <46404433+agsimette@users.noreply.github.com>
2023-03-16 16:18:53 -0300

    Primeiro Teste

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+
 
 function App() {
+
+const [contador, setContador] = useState(0);
+
+function handleButtonClick(){
+  setContador(contador + 1);
+}
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <Header title={contador}/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="button" value="click me" onClick={handleButtonClick} />
       </header>
     </div>
   );
